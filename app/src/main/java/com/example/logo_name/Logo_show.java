1410e4bda400368e37f0eedcd66e60476c77802a
Shayri <viranipradip100@gmail.com>
2023-05-19 12:01:47 +0530

    ten

---
+++ app/src/main/java/com/example/logo_name/Logo_show.java
@@ -0,0 +1,14 @@
+package com.example.logo_name;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Logo_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_show);
+    }
+}
